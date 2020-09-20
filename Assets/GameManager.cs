6644f6a1fb215c034bde3b217a9ca01e00125395
manoose47 <manoose47@gmail.com>
2020-09-20 19:48:34 +0100

    Add GameOver and Pause screen

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -13,6 +13,9 @@ public class GameManager : MonoBehaviour
     public static int leftScore;
     public static int rightScore;
     public Text scoreText;
+    public Text stateText;
+
+    private GameObject gameStateObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +33,10 @@ public class GameManager : MonoBehaviour
         paddle2.Init(false);
         leftScore = 0;
         rightScore = 0;
+
+        // hide the game over or paused text at runtime
+        gameStateObject = GameObject.FindGameObjectWithTag("ShowGameState");
+        gameStateObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -52,24 +59,47 @@ public class GameManager : MonoBehaviour
         UpdateScore(leftScore, rightScore);
     }
 
-    private void UpdateScore(int leftScore, int rightScore)
+    private string UpdateScore(int leftScore, int rightScore)
     {
         scoreText.text = string.Format("{0} : {1}", leftScore, rightScore);
         if (leftScore > rightScore)
         {
-            Debug.Log("Left player is winning: " + leftScore + ":" + rightScore);
+            return ("Left player wins: " + leftScore + ":" + rightScore);
         }
         else if (rightScore > leftScore)
         {
-            Debug.Log("Right player is winning: " + rightScore + ":" + leftScore);
+            return ("Right player wins: " + rightScore + ":" + leftScore);
         }
         else
         {
-            Debug.Log("The score is a draw: " + rightScore + ":" + leftScore);
+            return ("The score is a draw: " + rightScore + ":" + leftScore);
         }
     }
     public void Reset(GameObject gameObject)
     {
         gameObject.transform.position = resetPoint;
+    }
+
+    public void GameOver()
+    {
+        scoreText.text = UpdateScore(leftScore, rightScore);
+        stateText.text = "Game Over";
+        gameStateObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void TogglePause()
+    {
+        stateText.text = "Paused";
+        if (Time.timeScale == 1)
+        {
+            gameStateObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            gameStateObject.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -5,10 +5,20 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public Text timeText;
+    private GameManager gameManager;
 
     bool timerIsRunning = false;
     void Start()
     {
+        GameObject controller = GameObject.FindGameObjectWithTag("GameManager");
+        if (controller != null)
+        {
+            gameManager = controller.GetComponent<GameManager>();
+        }
+        else
+        {
+            Debug.Log("Cannot find game Manager");
+        }
         timerIsRunning = true;
     }
 
@@ -23,6 +33,10 @@ public class Timer : MonoBehaviour
         {
             if (timeRemaining > 0)
             {
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    gameManager.TogglePause();
+                }
                 timeRemaining -= Time.deltaTime;
                 DisplayTime(timeRemaining);
             }
@@ -32,6 +46,7 @@ public class Timer : MonoBehaviour
                 // lock time to 0, because update will keep running, and timer will likely be in negative numbers
                 timeRemaining = 0;
                 timerIsRunning = false;
+                gameManager.GameOver();
             }
         }
     }
